15a6a8349815574e7379b35f0fb396b3001bf1ec
batalawata <btlwtl@2930.se>
2022-04-15 08:54:23 +0200

    keep on

--- datalagring/lektioner/bonus/client/src/components/item/cards/Card.js
+++ datalagring/lektioner/bonus/client/src/components/item/cards/Card.js
@@ -1,12 +1,17 @@
 import css from './Table.module.css'
 
+// function swedify(input) {
+//     let options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
+//     return input.toLocaleString('se-SV', options)
+// }
+
 const Card = ({ item }) => {
     return (
         <tr className={ css.border }>
             <td className={ css.border }>{ item._id }</td>
-            <td className={ css.border }>{ item.name }</td>
-            <td className={ css.border }>{ item.age }</td>
-            <td className={ css.border }>{ item.gender }</td>
+            <td className={ css.border }>{ item.title }</td>
+            <td className={ css.border }>{ item.deadline }</td>
+            <td className={ css.border }>{ <input type="checkbox" checked={item.isDone} onChange={() => {}}/> }</td>
         </tr>
     )
 }
